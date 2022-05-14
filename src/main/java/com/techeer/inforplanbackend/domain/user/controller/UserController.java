0b1bb9572baad74926a5d99da54a8bd0de977cf6
hee <jkgt0521@naver.com>
2022-05-14 23:52:52 +0900

    Feat: #13 프로젝트 재구성 및 service, controller 작성

--- src/main/java/com/techeer/inforplanbackend/domain/user/controller/UserController.java
+++
@@ -1,4 +0,0 @@
-package com.techeer.inforplanbackend.domain.user.controller;
-
-public class UserController {
-}
--- src/main/java/com/techeer/inforplanbackend/domain/user/service/UserService.java
+++
@@ -1,4 +0,0 @@
-package com.techeer.inforplanbackend.domain.user.service;
-
-public class UserService {
-}
